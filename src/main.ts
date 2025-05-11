ffb84a626545d48e983c68c145c98ed23a53d055
RicardoTipan <ricardojaviertipan@gmail.com>
2025-05-10 22:07:34 -0500

    Configuracion de Variables de entorno Orders-MS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Console } from 'console';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  
+  const logger = new Logger('Orders-Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port);
+  Logger.log(`Microservice running on port ${envs.port}`);
+
 }
 bootstrap();
