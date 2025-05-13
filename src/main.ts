4dd5d0d9c0bdda1c663085ff7df7949d98f4ec29
RicardoTipan <ricardojaviertipan@gmail.com>
2025-05-12 21:45:53 -0500

    Configuracion de service ORDER-MS/dto y conexion con GATEWAY

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,19 @@ import { AppModule } from './app.module';
 import { envs } from './config';
 import { Console } from 'console';
 import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   
   const logger = new Logger('Orders-Main');
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
+  const app = await NestFactory.createMicroservice(AppModule,{
+    transport: Transport.TCP,
+    options:{
+      port:envs.port
+    }
+  });
+
+  await app.listen();
   Logger.log(`Microservice running on port ${envs.port}`);
 
 }
